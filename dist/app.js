7caaee57a2995f08e2d2fe8f203f9b81e348f103
harbapriyankapadmakar <130924686+harbapriyankapadmakar@users.noreply.github.com>
2023-12-15 15:05:46 +0530

    ts into node

--- dist/app.js
+++ dist/app.js
@@ -7,6 +7,6 @@ const express_1 = __importDefault(require("express"));
 const todos_1 = __importDefault(require("./routes/todos"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
-app.use(body_parser_1.default, json());
+app.use(body_parser_1.default.json());
 app.use(todos_1.default);
 app.listen(3000);
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -2,6 +2,8 @@ import {Router} from 'express';
 
 import {Todo} from '../models/todo';
 
+type RequestBody = {text: string};
+type RequestParams = {todoId:string};
 
 let todos:Todo[] = [];
 
@@ -12,6 +14,7 @@ router.get('/',(req,res,next) =>{
 });
 
 router.post('/todo',(req,res,next) =>{
+    const body = req.body as RequestBody;
     const newTodo:Todo ={
         id: new Date().toISOString(),
        text:req.body.text
@@ -21,17 +24,20 @@ router.post('/todo',(req,res,next) =>{
 });
 
 router.put('/todo/:todoId',(req,res,next) =>{
+    const params=req.params as RequestParams;
     const tid =req.params.todoId;
+    const body = req.body as RequestBody;
     const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
     if(todoIndex >= 0){
-        todos[todoIndex] = {id:todos[todoIndex].id,text:req.body.text};
+        todos[todoIndex] = {id:todos[todoIndex].id,text:body.text};
         return res.status(200).json({message:'Updated Todo',todos:todos});
     }
     res.status(404).json({message:'could not found Id'});
 });
 
 router.delete('/todo/:todoId',(req,res,next) =>{
-    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId );
+    const params=req.params as RequestParams;
+    todos = todos.filter(todoItem => todoItem.id !== params.todoId );
     return res.status(200).json({message:'Deleted Todo',todos:todos});
 });
 
